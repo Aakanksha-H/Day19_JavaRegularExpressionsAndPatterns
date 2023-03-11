d7aaa68020a3a233b068abe3279999bbea37397c
Akanksha-H <aakanksha0499@gmail.com>
2023-03-11 19:53:01 +0530

    Added: Enter name facility

---
+++ com/bridgelabz/javarejex/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.javarejex;
+
+public class UserRegistration {
+    public static void userRegistration(){
+        UserRegistrationCases userCases = new UserRegistrationCases();
+        userCases.enterFirstName();
+    }
+}
+
+
